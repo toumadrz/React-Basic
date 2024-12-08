222b3907daa8b57478d1d4bee571aabaa385f953
toumadrz <toumadrz@gmail.com>
2024-12-08 11:27:05 +0700

    แก้ Navbar ให้Responsive ไม่เลื่อนไปมา

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => (
-  <nav className="navbar navbar-dark bg-primary shadow mb-5">
+  <nav className="navbar navbar-dark bg-primary shadow mb-5 navbar-expand-lg">
     <div className="container">
-      <Link to="/" className="navbar-brand text-white">
+      <Link to="/" className="navbar-brand text-white mx-2 fs-4">
         <i className="fas fa-tasks me-2"></i>ระบบบัญชีรายรับ - รายจ่าย
       </Link>
-      <div className="navbar-nav d-flex flex-row gap-3">
-        <Link to="/" className="navbar-brand text-white">
-          <i className="fas fa-list-ul me-2"></i>ข้อมูลบัญชี
-        </Link>
-        <Link to="/add" className="navbar-brand text-white">
-          <i className="fas fa-plus-circle me-2"></i>บันทึกรายรับ - รายจ่าย
-        </Link>
+      
+      {/* เพิ่มปุ่มสำหรับหน้าจอเล็ก */}
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      
+      {/* เมนูปุ่ม */}
+      <div className="collapse navbar-collapse " id="navbarNav">
+      <div className="navbar-nav ms-auto fs-5 d-flex flex-row gap-lg-3 justify-content-around">
+          <Link to="/" className="nav-link text-white">
+            <i className="fas fa-list-ul me-2"></i>ข้อมูลบัญชี
+          </Link>
+          <Link to="/add" className="nav-link text-white">
+            <i className="fas fa-plus-circle me-2"></i>บันทึกรายรับ - รายจ่าย
+          </Link>
+        </div>
       </div>
     </div>
   </nav>
